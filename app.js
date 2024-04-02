c6b64c74e1d86b65d2e2241f190e2bb00cc0a77e
j-marcos-b <correodemarcosb@gmail.com>
2024-04-02 18:44:54 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const tarjeta1 = {
+    titulo: 'Viajar por el mundo',
+    imagen: 'img/viaje-mono.jpg',
+    texto: 'Me encanta visitar nuevos lugares. Acá estoy en Cancún con mi esposa Mamona y mi hijo Chupito'
+}
+
+const tarjeta2 = {
+    titulo: 'Robar cosas a los turistas',
+    imagen: 'img/botella-mono.jpg',
+    texto: 'Me divierto sacandole las cosas a los turistas cuando se distraen. Acá estoy tomando agua mineral. ¡Mmmmm que rica!'
+}
+
+const tarjeta3 = {
+    titulo: 'Ser muy travieso',
+    imagen: 'img/mono-moto.jpg',
+    texto: 'Como en la selva no hay mucho para hacer, me pongo a andar en moto. ¡Qué jodón que soy!'
+}
+
+const seccion3 = document.querySelector('.seccion-3');
+
+const contenido = 
+`<article>
+<h4>${tarjeta1.titulo}</h4>
+<img class="mono-hobbies" src="${tarjeta1.imagen}" alt="familia de monos">
+<p>${tarjeta1.texto}</p>
+</article>
+<article>
+<h4>${tarjeta2.titulo}</h4>
+<img class="mono-hobbies" src="${tarjeta2.imagen}" alt="mono tomando agua de una botella">
+<p>${tarjeta2.texto}</p>
+</article>
+<article>
+<h4>${tarjeta3.titulo}</h4>
+<img class="mono-hobbies" src="${tarjeta3.imagen}" alt="mono con pelota en la cabeza">
+<p>${tarjeta3.texto}</p>
+</article>`
+
+seccion3.innerHTML = contenido;
+
+
+
